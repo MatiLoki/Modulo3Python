0b28b3c858da7248a4b604a1e6668f02fcb2249a
matiasfsm <matiasfsm@gmail.com>
2024-03-08 16:56:36 -0300

    commiiit

---
+++ clasespython/listas.py
@@ -0,0 +1,11 @@
+"""
+una lista siempre se define entre corchetes (parentesis cuadrados)
+y cada elemento se separa con coma
+
+"""
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+datos_varios = [1, "patata", True, 3.0]
+print(datos_varios[1])
+lista_anidada = [1, "patata", ['perro', 'gato']]
+print(lista_anidada[2])
+print(lista[8])
--- desafio3sentencias/primeros_auxilios.py
+++ desafio3sentencias/primeros_auxilios.py
@@ -13,3 +13,16 @@ else :
     else :
         print("Administrar 5 ventilaciones y llamar a ambulancia")    
        
+resp_2=str(input("¿Signos de vida?")).lower()       
+if resp_2 == "si":
+    print("Reevaluar a la espera de la ambulancia")
+    resp_3=input("¿Llego la ambulancia?").lower()
+    if resp_3== "si" :
+        StopIteration
+else : 
+    print("Administrar compresiones torácicas hasta que llegue ambulancia")
+    resp_3=input("¿Llego la ambulancia?").lower()
+    if resp_3=="si":
+        StopIteration
+
+
