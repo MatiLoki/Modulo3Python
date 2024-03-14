91b936fa487821feed0ccbe00a717f8e6303e4a2
matiasfsm <matiasfsm@gmail.com>
2024-03-14 18:01:28 -0300

    commitaa

---
+++ Desafio6Estructuras/templates_quiz/level.py
@@ -0,0 +1,21 @@
+def choose_level(n_pregunta, p_level):
+    
+    # Construir lógica para escoger el nivel
+    ##################################################
+    if n_pregunta <= p_level:
+        level = "basicas"
+    elif n_pregunta <= p_level*2:
+        level = "intermedio"
+    else:
+        level = "avanzado"  
+    
+    ##################################################
+    
+    return level
+
+if __name__ == '__main__':
+    # verificar resultados
+    print(choose_level(2, 2)) # básicas
+    print(choose_level(3, 2)) # intermedias
+    print(choose_level(7, 2)) # avanzadas
+    print(choose_level(4, 3)) # intermedias
